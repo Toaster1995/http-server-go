f617f3f2df3205d455e754c38166237533b36e0d
Toaster1995 <lukasstuffer2908@gmail.com>
2025-01-07 22:40:59 +0100

    extract url path

--- app/server.go
+++ app/server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -19,12 +20,34 @@ func main() {
 	}
 	defer l.Close()
 
-	connection, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
-	defer connection.Close()
+	handleConn(l)
+}
 
-	connection.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+func handleConn(l net.Listener) {
+	for {
+		connection, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		buffer := make([]byte, 1024)
+		_, err = connection.Read(buffer)
+
+		if err != nil {
+			fmt.Println("couldn`t read from connection")
+			connection.Close()
+			continue
+		}
+
+		req := strings.Split(string(buffer), "\r\n")[0]
+		target := strings.Split(req, " ")[1]
+
+		if target != "/" {
+			connection.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+		} else {
+			connection.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+		}
+		connection.Close()
+	}
 }
